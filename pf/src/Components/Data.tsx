76a34edaa52477059c4c4a35cc01a70ca2dd305e
hijane0322 <hj960322@gmail.com>
2022-04-10 14:20:41 +0900

    address changed

--- pf/src/Components/Data.tsx
+++ pf/src/Components/Data.tsx
@@ -4,7 +4,7 @@ export const dbs = [{
     title: "Netflix",
     description: "Cloning one of the most famous wordwide streaming site",
     kind: "web app",
-    address:"https://jlee0505.github.io/NetflixClone/",
+    address:"https://jlee0505.github.io/notflix-clone/",
     images:["/images/netflix_1.png","/images/netflix_2.png", "/images/netflix_3.png", "/images/netflix_4.png"]
   },{
     id: "02",
